81219ead11cc0d1e5e8ed845e44d616c6f259267
VaLeoMe <102381636+VaLeoMe@users.noreply.github.com>
2023-05-23 16:03:20 +0200

    Update Application.java

--- src/main/Application.java
+++ src/main/Application.java
@@ -11,26 +11,34 @@ import static main.SSLCertificateHelper.getSSLCertificates;
 public class Application {
 
     public static void main(String[] args) {
-
         printWelcomeLine();
-        URL url = getURLFromUser();
-        Certificate[] certificates = getSSLCertificates(url);
-        while (certificates.length == 0) {
-            url = getNewURLFromUser();
-            certificates = getSSLCertificates(url);
-        }
-        printInfoOfSSLCertificates(certificates);
-        printInputInstructionsEnd();
-        boolean userWantsToContinue = checkIfUserWantsToContinue();
-
+        Certificate[] certificates = getValidSSLCertificates();
+        boolean userWantsToContinue = true;
         while (userWantsToContinue) {
-            printInputInstructionsBasic();
-            url = getURLFromUser();
-            certificates = getSSLCertificates(url);
+            while (certificates.length == 0) {
+                certificates = getValidSSLCertificates();
+            }
             printInfoOfSSLCertificates(certificates);
             printInputInstructionsEnd();
             userWantsToContinue = checkIfUserWantsToContinue();
+            certificates = new Certificate[0];
         }
     }
 
+    private static Certificate[] getValidSSLCertificates() {
+        boolean validCertificates = false;
+        URL url;
+        Certificate[] certificates = new Certificate[0];
+        while (!validCertificates) {
+            printInputInstructionsBasic();
+            url = getURLFromUser();
+            try {
+                certificates = getSSLCertificates(url);
+                validCertificates = true;
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return certificates;
+    }
 }
